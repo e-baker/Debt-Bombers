33cfbb0838e3613c7b2eeda60ce59e771c4d2bd9
Eric Baker <eric@ericbaker.me>
2015-10-10 01:41:41 -0400

    Initial Theme Setup
    Laying the Foundation

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php 
+/** 
+* Functions 
+* 
+* @package Debt Bombers 
+* @author Eric Baker <eric@trafficlight.me> 
+* @copyright Copyright (c) 2015, Allen Turner Law
+* 
+*/ 
+
+/** 
+* Theme Setup 
+* 
+* This setup function attaches all of the site-wide functions 
+* to the correct actions and filters. All the functions themselves 
+* are defined below this setup function. 
+* 
+*/ 
+
+add_action('genesis_setup','child_theme_setup', 15); 
+function child_theme_setup() { 
+
+}
+
+?>
